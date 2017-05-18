8d10243ec1449974885a79788fd077988b5a16f5
Shaiju E <eshaiju@gmail.com>
2017-05-18 19:55:27 +0530

    Added delete comment mutation

--- app/graphql/mutations/comment_mutations.rb
+++ app/graphql/mutations/comment_mutations.rb
@@ -3,13 +3,14 @@ module CommentMutations
   Create = GraphQL::Relay::Mutation.define do
     name "AddComment"
 
+    # Define input parameters
     input_field :articleId, !types.ID
     input_field :userId, !types.ID
     input_field :comment, !types.String
 
+    # Define return parameters
     return_field :article, ArticleType
     return_field :errors, types.String
-
 
     resolve ->(object, inputs, ctx) {
       article = Article.find_by_id(inputs[:articleId])
@@ -28,14 +29,15 @@ module CommentMutations
   Update = GraphQL::Relay::Mutation.define do
     name "UpdateComment"
 
+    # Define input parameters
     input_field :id, !types.ID
     input_field :comment, types.ID
     input_field :userId, types.ID
     input_field :articleId, types.ID
 
+    # Define return parameters
     return_field :comment, CommentType
     return_field :errors, types.String
-
 
     resolve ->(object, inputs, ctx) {
       comment = Comment.find_by_id(inputs[:id])
@@ -47,6 +49,29 @@ module CommentMutations
       else
         { errors: comment.errors.to_a }
       end
+    }
+  end
+
+  Destroy = GraphQL::Relay::Mutation.define do
+    name 'DestroyComment'
+    description 'Delete a comment and return post and deleted comment ID'
+
+    # Define input parameters
+    input_field :id, !types.ID
+
+    # Define return parameters
+    return_field :deletedId, !types.ID
+    return_field :article, ArticleType
+    return_field :errors, types.String
+
+    resolve ->(_obj, inputs, ctx) {
+      comment = Comment.find_by_id(inputs[:id])
+      return { errors: 'Comment not found' } if comment.nil?
+
+      article = comment.article
+      comment.destroy
+
+      { article: article.reload, deletedId: inputs[:id] }
     }
   end
 end
--- app/graphql/mutations/mutation_type.rb
+++ app/graphql/mutations/mutation_type.rb
@@ -3,4 +3,5 @@ MutationType = GraphQL::ObjectType.define do
   # Add the mutation's derived field to the mutation type
   field :addComment, field: CommentMutations::Create.field
   field :updateComment, field: CommentMutations::Update.field
+  field :destroyComment, field: CommentMutations::Destroy.field
 end
