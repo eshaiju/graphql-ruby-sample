0605ee6edbdc106650d326e8c3d6e5dbdeb684ab
Shaiju E <eshaiju@gmail.com>
2017-10-16 23:28:15 +0530

    Code cleaning- rubocop

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -1,9 +1,14 @@
 ArticleType = GraphQL::ObjectType.define do
-  name "Article"
+  name 'Article'
   field :id, types.Int
   field :title, types.String
   field :body, types.String
   field :comments, types[CommentType] do
     preload :comments
+  end
+  field :image, types.String do
+    resolve lambda { |obj, _args, _ctx|
+      obj.image.current_path
+    }
   end
 end
--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,9 +1,9 @@
 CommentType = GraphQL::ObjectType.define do
-  name "Comment"
+  name 'Comment'
   field :id, types.Int
   field :comment, types.String
   field :user, -> { UserType } do
-    resolve -> (obj, args, ctx) {
+    resolve lambda { |obj, _args, _ctx|
       RecordLoader.for(User).load(obj.user_id)
     }
   end
